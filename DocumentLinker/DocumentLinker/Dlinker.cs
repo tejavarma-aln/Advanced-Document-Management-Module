7d5cc454974ad2fa9edf18805d8104645f6f22f2
teja varma <52781687+tejavarma-aln@users.noreply.github.com>
2020-03-17 11:59:17 +0530

    Add files via upload

---
+++ DocumentLinker/DocumentLinker/Dlinker.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocumentLinker
+{
+    public class Dlinker
+    {
+
+        public String LauchDialog(int multiselect)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                OpenFileDialog of = new OpenFileDialog
+                {
+                    Title = "Select files to link",
+                    Multiselect = (multiselect == 1)
+                };
+                if (of.ShowDialog() == true)
+                {
+                    foreach (String f in of.FileNames)
+                    {
+                        sb.Append(f);
+                        sb.Append("|");
+                    }
+                    return sb.ToString();
+                }
+            }catch(Exception ex)
+            {
+                return "Error|" + ex.Message;
+            }
+
+            return "OPFAILED|";
+        }
+
+    }
+}
